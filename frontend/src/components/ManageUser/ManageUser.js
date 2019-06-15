33d30c6896991b2092e009b7e40c109f5a6606d1
Miguel Brito <miguelfbrito11@gmail.com>
2019-06-15 23:27:20 +0100

    Adição de filtro e menu à área da gestão de conta

--- frontend/src/components/ManageUser/ManageUser.js
+++ frontend/src/components/ManageUser/ManageUser.js
@@ -12,34 +12,31 @@ import Premium from './Premium/Premium';
 class CheckBetsSummary extends Component {
     constructor(props) {
         super(props);
-        this.state = { user: [], showEditSlip: false, showDetailsSlip: false, showMoneySlip: false, showPremium: true }
+        this.state = {
+            user: [], showEditSlip: false, showDetailsSlip: false, showMoneySlip: false, showPremium: true,
+            menus: ['Profile', 'Edit Profile', 'Deposit/Withdraw', 'Premium'], filter: 'Profile'
+        }
     }
 
     async componentDidMount() {
-        
+
         const t = 1;
 
         let userdetails = await Api.fetchUserDetails(1);
 
-        this.setState({user: userdetails});
+        this.setState({
+            user: userdetails
+        })
+    }
 
-
-        /*this.setState({
-            users: [
-                {
-                    "Username": "João",
-                    "Saldo": "30.00",
-                    "Nome": "João Pinto",
-                    "Email": "user@email.com",
-                    "Premium": "Não"
-                }
-            ]
-        })*/
-
+    changeFilter = (menu) => {
+        this.setState({ filter: menu });
     }
 
     render() {
         //TODO: Adicionar um scroll para os eventos
+
+        let { filter, menus } = this.state;
 
         const editSlipSection = (
             <div className="user">
@@ -61,25 +58,26 @@ class CheckBetsSummary extends Component {
                 <Premium user={this.state.user} />
             </div>);
 
-        return ( 
+
+        return (
             <div className="user-title">
-                <p className="management-div">Account Management</p>
+                <div className="top-bar">
+                    <p className="management-div">Account Management</p>
+                </div>
+
                 <div className="user-container shadow">
+                    <div className="top-bar-menu">
+                        {menus.map(menu => (
+                            <p className={filter === menu ? 'top-bar-menu-active' : ''} onClick={() => this.changeFilter(menu)}>{menu}</p>
+                        ))}
+                    </div>
 
-
-                    {/* Carousel 4 or 5 games */}
-                    {/* <CurrentEventsCarousel /> */}
-
-                    {/* List all events */}
-
-                    <FilterOptions />
-
-                        <div >
-                            {this.state.showDetailsSlip ? detailsSlipSection : ''}
-                            {this.state.showEditSlip ? editSlipSection : ''}
-                            {this.state.showMoneySlip ? moneySlipSection : ''}
-                            {this.state.showPremium ? premiumSlipSection : ''}
-                        </div>
+                    <div >
+                        {filter === 'Profile' ? detailsSlipSection : ''}
+                        {filter === 'Edit Profile' ? editSlipSection : ''}
+                        {filter === 'Deposit/Withdraw' ? moneySlipSection : ''}
+                        {filter === 'Premium' ? premiumSlipSection : ''}
+                    </div>
 
                 </div>
             </div>
