8e1b0c95fe92da09bae4d86f48d2fa77720517e3
Miguel Brito <miguelfbrito11@gmail.com>
2019-06-16 23:03:55 +0100

    Remoção de projetos base dos microserviços

--- microservices/constants.js
+++
@@ -1,8 +0,0 @@
-//const Constants = module.exports;
-//
-//Constants.DB_EVENT_HOST = process.env.DB_EVENT_HOST || 'isto é um teste';
-//
-
-const Common = module.exports
-
-Common.host = 'http://localhost:8000'
